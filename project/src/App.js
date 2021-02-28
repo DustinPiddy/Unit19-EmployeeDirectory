9a469a81fedac0f94525a1c1ea5f632ff6b301b8
Dustin Pidcock <dustygen@gmail.com>
2021-02-27 16:06:10 -0800

    did what I could in styling, bootstrap in react is confusing

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getRandomUsers } from "./API";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [users, setUsers] = useState({ userData: [] });
@@ -48,7 +49,7 @@ function App() {
    const handleSort = (key) => {
      const sorted = filteredUsers.userData.sort((a, b) => {
        if (a[key] < b[key]) return -1 * sortDirection;
-       if (b[key] > b[key]) return 1 * sortDirection;
+       if (b[key] > a[key]) return 1 * sortDirection;
        return 0;
      });
      setFilteredUsers({userData: sorted});
@@ -56,11 +57,71 @@ function App() {
    };
    return (
      <>
-     <h1 style={{textAlign: 'center'}} className="bg-light p-5">
+     <body type="header" className="bg-blue">
+     <h1 type="header" style={{textAlign: 'center'}} className="p-3">
        Welcome!
-      
      </h1>
+     <div style={{ marginLeft: "10%", width: "50%"}}
+     className="btn-group" role="group" aria-label="Basic Example"
+     >
+       <div className="input-group ml-2 bg-blue">
+         <span>
+           Employee search
+         </span>
+         <input onChange={({target: {value}}) =>
+        handleFilterUsers(value.toLocaleLowerCase())
+         }
+        placeholder="or click a header" type="text" className="formControl ml-3"/>
+       </div>
+       <button type="button" onclick={() => getAndSetUsers(20)} className= "btn btn-secondary">
+          Find a little
+       </button>
+       <button type="button" onclick={() => getAndSetUsers(20)} className= "btn btn-success">
+          Find a chunk
+       </button>
+       <button type="button" onclick={() => getAndSetUsers(20)} className= "btn btn-danger">
+          Find a heckin lot
+       </button>
 
+     </div>
+     <table className="table-primary mt-2" style={{width: "70%", marginLeft: "15%"}}>
+       <thread>
+         <tr>
+           <th scope="col"></th>
+            <th onClick={()=> handleSort("firstName")}>
+              First
+            </th>
+            <th onClick={()=> handleSort("lastName")}>
+              Last
+            </th>
+            <th onClick={()=> handleSort("age")}>
+              Age
+            </th>
+            <th onClick={()=> handleSort("phone")}>
+              Phone
+            </th>
+            <th onClick={()=> handleSort("email")}>
+              Email Address
+            </th>
+         </tr>
+       </thread>
+       <tbody>
+         {filteredUsers.userData.map((user, i) =>(
+           <tr key={`user${i}`}>
+             <th scope="row">
+               <img src={user.picture} alt={"user"}/>
+             </th>
+             <td>{user.firstName}</td>
+             <td>{user.lastName}</td>
+             <td>{user.age}</td>
+             <td>{user.phone}</td>
+             <td>{user.email}</td>
+           </tr>
+         ))}
+       </tbody>
+      
+     </table>
+ </body>
      </>
    )
 }
